c0b74401f7651c93e1ba8d095035f599bec4f0ec
sujay <sujaydey641@gmail.com>
2026-01-25 20:24:40 +0530

    Skip: .secrets repo

--- flake.nix
+++ flake.nix
@@ -25,10 +25,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    dot-secrets = {
-      url = "git+ssh://git@github.com/pirate-17/.secrets.git";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    # dot-secrets = {
+    #   url = "git+ssh://git@github.com/pirate-17/.secrets.git";
+    #   inputs.nixpkgs.follows = "nixpkgs";
+    # };
 
     zen-browser = {
       url = "github:0xc000022070/zen-browser-flake";
